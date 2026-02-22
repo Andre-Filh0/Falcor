287d80a20e4045e949f19a3cb78297d0584413ea
Andre-Filh0 <brfalcorvision@gmail.com>
2026-02-22 17:43:35 -0400

    Updating the Logger

--- common/core/src/Logger.hpp
+++ common/core/src/Logger.hpp
@@ -1,3 +1,5 @@
+#pragma once
+
 #ifndef FALCOR_LOGGER_HPP
 #define FALCOR_LOGGER_HPP
 
@@ -9,116 +11,119 @@
 #include <utility>
 #include <string_view>
 #include <mutex>
+#include <chrono>
+#include <ctime>
+#include <source_location>
 
 namespace Falcor {
 
-// ============================================================
-// Inicialização segura do Quill (roda só uma vez)
-// ============================================================
-inline void ensure_quill_started()
-{
+inline void ensure_quill_started() {
     static std::once_flag flag;
-    std::call_once(flag, []() {
-        quill::start();
-    });
+    std::call_once(flag, []() { quill::start(); });
 }
 
-// ============================================================
-// Base Logger
-// ============================================================
+namespace Color {
+    inline constexpr std::string_view Reset      = "\033[0m";
+    inline constexpr std::string_view Dim        = "\033[90m";   
+    inline constexpr std::string_view Bold       = "\033[1m";
+    inline constexpr std::string_view Blue       = "\033[34m";   
+    inline constexpr std::string_view Cyan       = "\033[36m";   
+    inline constexpr std::string_view Red        = "\033[91m";   
+    inline constexpr std::string_view Magenta    = "\033[35m";   
+    inline constexpr std::string_view File       = "\033[38;5;242m"; 
+}
+
+/**
+ * @brief Estrutura para capturar a string de formato E a localização.
+ * Isso resolve o conflito de templates com argumentos variádicos.
+ */
+struct LogFormat {
+    std::string_view fmt;
+    std::source_location loc;
+
+    // Construtor implícito para aceitar strings e capturar a origem
+    LogFormat(const char* s, std::source_location l = std::source_location::current())
+        : fmt(s), loc(l) {}
+        
+    LogFormat(std::string_view s, std::source_location l = std::source_location::current())
+        : fmt(s), loc(l) {}
+};
+
 class Logger {
 public:
-    explicit Logger(quill::Logger* p_logger)
-        : m_logger(p_logger)
-    {
+    explicit Logger(quill::Logger* p_logger) : m_logger(p_logger) {
         ensure_quill_started();
     }
 
     virtual ~Logger() = default;
 
     template <typename... Args>
-    void log_info(std::string_view format_str, Args&&... args)
-    {
-        std::string raw_msg =
-            fmtquill::v10::format(
-                fmtquill::v10::runtime(format_str),
-                std::forward<Args>(args)...
-            );
+    void log_info(LogFormat format, Args&&... args) {
+        std::string raw_msg = fmtquill::v10::format(fmtquill::v10::runtime(format.fmt), std::forward<Args>(args)...);
+        
+        std::cout << decorate_info() << format_loc(format.loc) << " " << raw_msg << Color::Reset << std::endl;
 
-        std::cout << decorate_info() << raw_msg << std::endl;
-
-        if (m_logger) {
-            QUILL_LOG_INFO(m_logger, "{}", raw_msg);
-        }
-
-        on_log_info(raw_msg);
+        if (m_logger) QUILL_LOG_INFO(m_logger, "[{}:{}] {}", format.loc.file_name(), format.loc.line(), raw_msg);
     }
 
     template <typename... Args>
-    void log_error(std::string_view format_str, Args&&... args)
-    {
-        std::string raw_msg =
-            fmtquill::v10::format(
-                fmtquill::v10::runtime(format_str),
-                std::forward<Args>(args)...
-            );
+    void log_error(LogFormat format, Args&&... args) {
+        std::string raw_msg = fmtquill::v10::format(fmtquill::v10::runtime(format.fmt), std::forward<Args>(args)...);
 
-        std::cerr << decorate_error() << raw_msg << std::endl;
+        std::cerr << decorate_error() << format_loc(format.loc) << " " << Color::Bold << raw_msg << Color::Reset << std::endl;
 
-        if (m_logger) {
-            QUILL_LOG_ERROR(m_logger, "{}", raw_msg);
-        }
-
-        on_log_error(raw_msg);
+        if (m_logger) QUILL_LOG_ERROR(m_logger, "[{}:{}] {}", format.loc.file_name(), format.loc.line(), raw_msg);
     }
 
 protected:
     virtual std::string decorate_info() = 0;
     virtual std::string decorate_error() = 0;
 
-    virtual void on_log_info(const std::string&) {}
-    virtual void on_log_error(const std::string&) {}
+    inline std::string format_loc(const std::source_location& loc) const {
+        std::string_view file = loc.file_name();
+        auto last_slash = file.find_last_of("\\/");
+        if (last_slash != std::string_view::npos) file.remove_prefix(last_slash + 1);
+        
+        return fmtquill::v10::format(" {}{}:{}{}", Color::File, file, loc.line(), Color::Reset);
+    }
+
+    inline std::string get_timestamp() const {
+        auto now = std::chrono::system_clock::now();
+        auto in_time_t = std::chrono::system_clock::to_time_t(now);
+        std::tm tm_struct;
+        localtime_r(&in_time_t, &tm_struct);
+
+        return fmtquill::v10::format("{}[{:02d}/{:02d}/{} {:02d}:{:02d}:{:02d}]{}", 
+            Color::Dim, tm_struct.tm_mday, tm_struct.tm_mon + 1, tm_struct.tm_year + 1900,
+            tm_struct.tm_hour, tm_struct.tm_min, tm_struct.tm_sec, Color::Reset);
+    }
 
 private:
     quill::Logger* m_logger;
 };
 
-// ============================================================
-// GENERAL LOGGER
-// ============================================================
 class GeneralLogger : public Logger {
-public:
-    explicit GeneralLogger(quill::Logger* logger)
-        : Logger(logger) {}
-
+public: using Logger::Logger;
 protected:
-    std::string decorate_info() override {
-        return "\033[1;32m[General]\033[0m ";
+    std::string decorate_info() override { 
+        return fmtquill::v10::format("{} {}{}[INFO]{}", get_timestamp(), Color::Bold, Color::Cyan, Color::Reset); 
     }
-
-    std::string decorate_error() override {
-        return "\033[1;31m[General]\033[0m ";
+    std::string decorate_error() override { 
+        return fmtquill::v10::format("{} {}{}[WARN]{}", get_timestamp(), Color::Bold, Color::Red, Color::Reset); 
     }
 };
 
-// ============================================================
-// SYSTEM LOGGER
-// ============================================================
 class SystemLogger : public Logger {
-public:
-    explicit SystemLogger(quill::Logger* logger)
-        : Logger(logger) {}
-
+public: using Logger::Logger;
 protected:
-    std::string decorate_info() override {
-        return "\033[1;34m[System]\033[0m ";
+    std::string decorate_info() override { 
+        return fmtquill::v10::format("{} {}{}[SYS ]{}", get_timestamp(), Color::Bold, Color::Blue, Color::Reset); 
     }
-
-    std::string decorate_error() override {
-        return "\033[1;35m[System Critical]\033[0m ";
+    std::string decorate_error() override { 
+        return fmtquill::v10::format("{} {}{}[CRIT]{}", get_timestamp(), Color::Bold, Color::Magenta, Color::Reset); 
     }
 };
 
 } // namespace Falcor
 
-#endif // FALCOR_LOGGER_HPP
+#endif
--- common/pipeline/src/main.cpp
+++ common/pipeline/src/main.cpp
@@ -3,17 +3,17 @@
 
 #include <cstdint>
 
-#define DEVICE_LIST_UPDATE_TIMEOUT 1000 /* millisecond */
+#define DEVICE_LIST_UPDATE_TIMEOUT  1000  /* millisecond */
 
-// Forward declarations
+// --- Protótipos ---
 DcError PrintSystemInfo(DcSystem system, Falcor::GeneralLogger& logger);
 DcError PrintInterfaceInfo(DcInterface interface, Falcor::GeneralLogger& logger);
 DcError PrintDeviceInfo2(DcDevice device, int64_t device_index, Falcor::GeneralLogger& logger);
 
 int main()
 {
-    // Create Quill logger
-    auto* quill_logger = quill::create_logger("general");
+    // Inicia o Quill
+    auto* quill_logger = quill::create_logger("GENERAL");
     Falcor::GeneralLogger logger(quill_logger);
 
     DcSystem system;
@@ -22,129 +22,98 @@ int main()
     size_t interface_count = 0;
     size_t device_count = 0;
 
-    RETURN_IF_ERROR(DcSystemCreate(&system));
+    // Criação do Sistema
+    if (DcSystemCreate(&system) != DC_ERROR_SUCCESS) {
+        logger.log_error("Falha ao criar DcSystem");
+        return -1;
+    }
+
     PrintSystemInfo(system, logger);
 
-    // Update interface list
-    RETURN_IF_ERROR(DcSystemUpdateInterfaceList(system, nullptr, DC_INFINITE));
+    // Atualiza lista de interfaces
+    DcSystemUpdateInterfaceList(system, NULL, DC_INFINITE);
+    DcSystemGetInterfaceCount(system, &interface_count);
 
-    RETURN_IF_ERROR(DcSystemGetInterfaceCount(system, &interface_count));
-    logger.log_info("Found {} interfaces", static_cast<uint32_t>(interface_count));
+    logger.log_info("Found {} interfaces", (uint32_t)interface_count);
 
     for (uint32_t i = 0; i < interface_count; i++)
     {
         logger.log_info("Interface {}", i);
 
-        RETURN_IF_ERROR(DcSystemGetInterface(system, i, &interface));
-        PrintInterfaceInfo(interface, logger);
+        if (DcSystemGetInterface(system, i, &interface) == DC_ERROR_SUCCESS) {
+            PrintInterfaceInfo(interface, logger);
 
-        // Open interface and update device list
-        RETURN_IF_ERROR(DcInterfaceOpen(interface));
-        RETURN_IF_ERROR(DcInterfaceUpdateDeviceList(interface, nullptr, DEVICE_LIST_UPDATE_TIMEOUT));
+            if (DcInterfaceOpen(interface) == DC_ERROR_SUCCESS) {
+                DcInterfaceUpdateDeviceList(interface, NULL, DEVICE_LIST_UPDATE_TIMEOUT);
+                DcInterfaceGetDeviceCount(interface, &device_count);
+                
+                logger.log_info("  Found {} device(s) on interface", (uint32_t)device_count);
 
-        DcInterfaceGetDeviceCount(interface, &device_count);
-        logger.log_info("  Found {} device(s) on the interface", static_cast<uint32_t>(device_count));
-
-        for (uint32_t j = 0; j < device_count; j++)
-        {
-            logger.log_info("    Device {}", j);
-
-            RETURN_IF_ERROR(DcInterfaceGetDevice(interface, j, &device));
-            RETURN_IF_ERROR(PrintDeviceInfo2(device, j, logger));
+                for (uint32_t j = 0; j < device_count; j++)
+                {
+                    logger.log_info("    Device {}", j);
+                    if (DcInterfaceGetDevice(interface, j, &device) == DC_ERROR_SUCCESS) {
+                        PrintDeviceInfo2(device, j, logger);
+                    }
+                }
+                DcInterfaceClose(interface);
+            }
         }
     }
 
-    RETURN_IF_ERROR(DcSystemDestroy(system));
-
+    DcSystemDestroy(system);
     logger.log_info("Finished device enumeration");
 
     return 0;
 }
 
-// ============================================================
-// Print system info
-// ============================================================
+// --- Implementações (O que estava faltando para o Linker) ---
+
 DcError PrintSystemInfo(DcSystem system, Falcor::GeneralLogger& logger)
 {
     char buffer[256] = {0};
-    size_t size;
+    size_t size = sizeof(buffer);
+
+    if (DcSystemGetInfo(system, DC_SYSTEM_INFO_VENDOR, buffer, &size) == DC_ERROR_SUCCESS)
+        logger.log_info("System Vendor: {}", buffer);
 
     size = sizeof(buffer);
-    RETURN_IF_ERROR(DcSystemGetInfo(system, DC_SYSTEM_INFO_VENDOR, buffer, &size));
-    logger.log_info("System Vendor: {}", buffer);
-
-    size = sizeof(buffer);
-    RETURN_IF_ERROR(DcSystemGetInfo(system, DC_SYSTEM_INFO_MODEL, buffer, &size));
-    logger.log_info("System Model: {}", buffer);
-
-    size = sizeof(buffer);
-    RETURN_IF_ERROR(DcSystemGetInfo(system, DC_SYSTEM_INFO_VERSION, buffer, &size));
-    logger.log_info("System Version: {}", buffer);
+    if (DcSystemGetInfo(system, DC_SYSTEM_INFO_MODEL, buffer, &size) == DC_ERROR_SUCCESS)
+        logger.log_info("System Model: {}", buffer);
 
     return DC_ERROR_SUCCESS;
 }
 
-// ============================================================
-// Print interface info
-// ============================================================
 DcError PrintInterfaceInfo(DcInterface interface, Falcor::GeneralLogger& logger)
 {
     char buffer[256] = {0};
-    size_t size;
+    size_t size = sizeof(buffer);
+
+    if (DcInterfaceGetInfo(interface, DC_INTERFACE_INFO_ID, buffer, &size) == DC_ERROR_SUCCESS)
+        logger.log_info("  Interface ID: {}", buffer);
 
     size = sizeof(buffer);
-    RETURN_IF_ERROR(DcInterfaceGetInfo(interface, DC_INTERFACE_INFO_ID, buffer, &size));
-    logger.log_info("  Interface ID: {}", buffer);
-
-    size = sizeof(buffer);
-    RETURN_IF_ERROR(DcInterfaceGetInfo(interface, DC_INTERFACE_INFO_DISPLAY_NAME, buffer, &size));
-    logger.log_info("  Interface Display Name: {}", buffer);
+    if (DcInterfaceGetInfo(interface, DC_INTERFACE_INFO_DISPLAY_NAME, buffer, &size) == DC_ERROR_SUCCESS)
+        logger.log_info("  Interface Display Name: {}", buffer);
 
     return DC_ERROR_SUCCESS;
 }
 
-// ============================================================
-// Print device info
-// ============================================================
 DcError PrintDeviceInfo2(DcDevice device, int64_t device_index, Falcor::GeneralLogger& logger)
 {
     char buffer[256] = {0};
-    size_t size;
-    DcInterface parent;
-    DcNodeList nodelist;
-    DcNode node;
-    DcIntegerNode device_selector;
+    size_t size = sizeof(buffer);
+
+    if (DcDeviceGetInfo(device, DC_DEVICE_INFO_VENDOR, buffer, &size) == DC_ERROR_SUCCESS)
+        logger.log_info("    Device Vendor: {}", buffer);
 
     size = sizeof(buffer);
-    RETURN_IF_ERROR(DcDeviceGetInfo(device, DC_DEVICE_INFO_VENDOR, buffer, &size));
-    logger.log_info("    Device Vendor: {}", buffer);
+    if (DcDeviceGetInfo(device, DC_DEVICE_INFO_MODEL, buffer, &size) == DC_ERROR_SUCCESS)
+        logger.log_info("    Device Model: {}", buffer);
 
     size = sizeof(buffer);
-    RETURN_IF_ERROR(DcDeviceGetInfo(device, DC_DEVICE_INFO_MODEL, buffer, &size));
-    logger.log_info("    Device Model: {}", buffer);
-
-    size = sizeof(buffer);
-    RETURN_IF_ERROR(DcDeviceGetInfo(device, DC_DEVICE_INFO_SERIAL_NUMBER, buffer, &size));
-    logger.log_info("    Serial Number: {}", buffer);
-
-    size = sizeof(buffer);
-    RETURN_IF_ERROR(DcDeviceGetInfo(device, DC_DEVICE_INFO_VERSION, buffer, &size));
-    logger.log_info("    Version: {}", buffer);
-
-    RETURN_IF_ERROR(DcDeviceGetParent(device, &parent));
-    RETURN_IF_ERROR(DcInterfaceGetNodeList(parent, &nodelist));
-    RETURN_IF_ERROR(DcNodeListGetNode(nodelist, "DeviceSelector", &node));
-    device_selector = DcNodeCastToIntegerNode(node);
-
-    RETURN_IF_ERROR(DcIntegerNodeSetValue(device_selector, device_index));
-
-    size = sizeof(buffer);
-    DcNodeListGetValue(nodelist, "GevDeviceMACAddress", buffer, &size);
-    logger.log_info("    Device MAC: {}", buffer);
-
-    size = sizeof(buffer);
-    DcNodeListGetValue(nodelist, "GevDeviceIPAddress", buffer, &size);
-    logger.log_info("    Device IP: {}", buffer);
+    if (DcDeviceGetInfo(device, DC_DEVICE_INFO_SERIAL_NUMBER, buffer, &size) == DC_ERROR_SUCCESS)
+        logger.log_info("    Serial Number: {}", buffer);
 
     return DC_ERROR_SUCCESS;
 }
